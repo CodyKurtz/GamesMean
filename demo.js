1d3507c5b076db704f403720678a59f7cb109bdc
root <root@localhost.localdomain>
2016-08-20 17:51:15 +0000

    Delete demo.js

--- demo.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("GamesApp", []);
-app.controller("GamesController", function($scope) {
-	var game = {name:"",platform:""}
-	$scope.game = game;
-	$scope.displayGame = function(){
-	 return $scope.game.name + " " + $scope.game.platform;	
-}
-});
