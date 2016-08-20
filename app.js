ffee168af1797d31e4e5c01dbb6d786b6093fcff
root <root@localhost.localdomain>
2016-08-20 21:04:33 +0000

    Login/Signup

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt-nodejs');
 var app = express();
 
-var mongoose = require('mongoose');
+
 require('./models/games');
+require('./models/users');
 
 mongoose.connect('mongodb://localhost/gamesDB', function(err) {
   if(err) {
@@ -18,6 +23,10 @@ mongoose.connect('mongodb://localhost/gamesDB', function(err) {
   }
 });
 
+var index = require('./routes/index');
+var api = require('./routes/api');
+var authenticate = require('./routes/authenticate')(passport);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,15 +35,21 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({secret: 'keyboard cat'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
-var index = require('./routes/index');
-var api = require('./routes/api');
+
 app.use('/', index);
 app.use('/api', api);
+app.use('/auth', authenticate);
+
+var initPassport = require('./passport-initialize');
+initPassport(passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/gamesApp.js
+++ public/javascripts/gamesApp.js
@@ -1,4 +1,13 @@
-var app = angular.module("gameApp", ['ngRoute', 'ngResource']);
+var app = angular.module("gameApp", ['ngRoute', 'ngResource']).run(function($http,$rootScope) {
+		$rootScope.authenticated = false;
+		$rootScope.current_user = 'Guest';
+		
+		$rootScope.signout = function(){
+			$http.get('auth/signout');
+			$rootScope.authenticated = false;
+			$rootScope.current_user = 'Guest';
+		};
+});
 
 app.config(function($routeProvider){
 	$routeProvider
@@ -9,13 +18,21 @@ app.config(function($routeProvider){
 		.when('/trophies', {
 			templateUrl: 'trophies.html',
 		})
+		.when('/signin', {
+			templateUrl: 'signin.html',
+			controller: 'authController'
+		})
+		.when('/signup', {
+			templateUrl: 'signup.html',
+			controller: 'authController'
+		})
 });
 
 app.factory('gameService', function($resource){
 	return $resource('/api/games');
 });
 
-app.controller("gameController", function($scope, $http, gameService){
+app.controller("gameController", function($scope, gameService){
 	$scope.games = [];
 	$scope.newGame = {name: '', platform: ''};
 	
@@ -27,4 +44,35 @@ app.controller("gameController", function($scope, $http, gameService){
 			$scope.newGame = {name: '', platform:''};
 		});
 	}
+});
+
+app.controller('authController', function($scope,$http,$location,$rootScope){
+	$scope.user = {username: '', password: ''};
+	$scope.msg='';
+	
+	$scope.signin = function(){
+		$http.post('/auth/signin', $scope.user).success(function(response){
+			if(response.state == 'success'){
+				$rootScope.authenticated = true;
+				$rootScope.current_user = response.user.username;
+				$location.path('/');
+			}
+			else {
+				$scope.msg = response.message;
+			}
+		});
+	};
+	
+	$scope.signup = function(){
+		$http.post('/auth/signup', $scope.user).success(function(response){
+			if(response.state == 'success'){
+				$rootScope.authenticated = true;
+				$rootScope.current_user = response.user.username;
+				$location.path('/');
+			}
+			else {
+				$scope.msg = response.message;
+			}
+		});
+	};
 });
